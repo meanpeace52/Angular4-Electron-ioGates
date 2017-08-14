fe9a965ab755325019918fedf4cf8331dfd9da5c
Hamza Waqas <hamza@getcilantro.com>
2017-08-14 05:27:15 +0000

    removed build/ files

--- build/src/types/download_command_input.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CommandDownloadInput {
-}
-exports.CommandDownloadInput = CommandDownloadInput;
-//# sourceMappingURL=download_command_input.js.map
--- build/src/types/files.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Files {
-}
-exports.Files = Files;
-//# sourceMappingURL=files.js.map
