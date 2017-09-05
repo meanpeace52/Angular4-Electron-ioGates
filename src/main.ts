295a1b9c038dfbde99816a097c89f125c0e5ab76
Hamza Waqas <hamza@getcilantro.com>
2017-09-05 19:39:51 +0000

    fixed command formation

--- src/main.ts
+++ src/main.ts
@@ -20,12 +20,12 @@ sequelize.addModels([Type.File, Type.Share]);
 
 sequelize.sync().then(() => {
   global['_DB'] = sequelize;
-  
+
   const commands = vorpal();
   commands
     .command('download [dir] [url]', 'Download folder from Share URL')
-    .option('-m', '--monitor', 'Shows download progress')
-    .option('-v', '--verbose', 'Shows debug logs')
+    .option('-m, --monitor', 'Shows download progress')
+    .option('-v, --verbose', 'Shows debug logs')
     .option('-w, --watch', 'Watch for changes and auto-download')
     .action(downloadComand);
   commands
@@ -33,7 +33,7 @@ sequelize.sync().then(() => {
     .show()
     .parse(process.argv);
   commands.commands = commands.commands;
-  
+
   module.exports = commands;
 }).error((error) => {
   console.log(error);
