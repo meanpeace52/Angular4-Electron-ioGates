2e25588a58570eb4d201392e4d45bc1867bc65ec
solar-dev23 <solar.dev023@gmail.com>
2017-11-22 16:19:58 +0100

    Share Model test for add model and save.

--- core/__tests__/types/models/share.spec.ts
+++ core/__tests__/types/models/share.spec.ts
@@ -1,48 +1,35 @@
-import * as assert from 'assert';
 import {Sequelize} from 'sequelize-typescript';
 import {File} from '../../../types/models/file';
 import {Share} from '../../../types/models/share';
 import {Chunk} from '../../../types/models/chunk';
 
 describe('Share', () => {
-  beforeAll(() => {
-    const db = new Sequelize({
-      name: 'sharetest',
-      dialect: 'sqlite',
-      username: 'root',
-      password: '',
-      logging: false,
-      storage: './sharetest.sqlite', // change this with your absolute path.
-      pool: {
-        max: 1,
-      },
-      operatorsAliases: false,
-    });
-
-    db.addModels([File, Share, Chunk]);
-
-    return db.sync().then(() => {
-      return;
-    });
+  let db = new Sequelize({
+    name: 'sharetest',
+    dialect: 'sqlite',
+    username: 'root',
+    password: '',
+    logging: false,
+    storage: ':memory:', // change this with your absolute path.
+    pool: {
+      max: 1,
+    },
+    operatorsAliases: false,
   });
 
-  describe('Save', () => {
-    const share = new Share({
-      dir: './foo/bar',
-      url: 'https://iogates.com',
-      token: '123abc',
-      complete: 0,
-      direction: Share.DIRECT_DOWNLOAD,
-    });
+  db.addModels([File, Share, Chunk]);
 
-    return share.save();
-  /*
-    return Share.create({
-      dir: './foo/bar',
-      url: 'https://iogates.com',
-      token: '123abc',
-      complete: 0,
-      direction: Share.DIRECT_DOWNLOAD,
-    });*/
+  test('Save', (done: Function) => {
+    return db.sync().then(() => {
+      const share = new Share({
+        dir: './foo/bar',
+        url: 'https://iogates.com',
+        token: '123abc',
+        complete: 0,
+        direction: Share.DIRECT_DOWNLOAD,
+      });
+
+      share.save().then(() => {done()});
+    });
   });
 });
